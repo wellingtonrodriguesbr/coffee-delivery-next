d3e90c16911b01d273deac8a3b68da224a42e319
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-04-20 18:35:50 -0300

    feat: add Link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
-import logo from "../assets/Logo.svg";
-import location from "../assets/icons/location.svg";
-import { MapPin, ShoppingCartSimple } from "@phosphor-icons/react";
-import { ButtonCart } from "./ButtonCart";
-import { useCart } from "@/contexts/CartContext";
 import Link from "next/link";
+import logo from "../assets/Logo.svg";
+import { MapPin, ShoppingCartSimple } from "@phosphor-icons/react";
+import { useCart } from "@/contexts/CartContext";
 
 export function Header() {
   const { cartQuantity } = useCart();
@@ -22,14 +20,16 @@ export function Header() {
           </div>
           <div className="relative group">
             {cartQuantity > 0 ? (
-              <p className="absolute -top-1 -right-2 text-[0.625rem] w-3 h-3 p-2 text-white rounded-full bg-yellow-dark flex justify-center items-center font-bold group-hover:bg-yellow-light group-hover:text-yellow-dark transition-colors">
+              <p className="absolute -top-2 -right-2 text-xs w-3 h-3 p-[10px] text-white rounded-full bg-yellow-dark flex justify-center items-center font-bold group-hover:bg-yellow-light group-hover:text-yellow-dark transition-colors">
                 {cartQuantity}
               </p>
             ) : null}
-            <ButtonCart
+            <Link
+              href="/checkout"
               className="flex justify-center items-center rounded-md text-yellow-dark bg-yellow-light p-2 group-hover:bg-yellow-dark group-hover:text-white transition-colors"
-              icon={<ShoppingCartSimple size={22} weight="fill" />}
-            />
+            >
+              <ShoppingCartSimple size={22} weight="fill" />
+            </Link>
           </div>
         </div>
       </div>
